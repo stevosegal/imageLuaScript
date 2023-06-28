c03cd5a232eac311c4aa086c8600f8235626270f
stevosegal <135548120+stevosegal@users.noreply.github.com>
2023-06-28 11:02:01 +1000

    Update LuaCode.lua

--- Asseto/LuaCode.lua
+++ Asseto/LuaCode.lua
@@ -3,7 +3,7 @@ local timer = 12
 
 --image_0 is used as the rules splash screen
 local image_0 = {
-    ['src'] = 'https://i.imgur.com/iu7I7jh.png',
+    ['src'] = 'https://i.imgur.com/8qAfNvD.png',
     ['sizeX'] = 546, --size of your image in pixels
     ['sizeY'] = 241, --size of your image in pixels
     ['paddingX'] = screensize.x/2-546/2, --this makes it sit in the centre of the screen
