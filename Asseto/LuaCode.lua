3589bdc426b2a540972f03e2825d02cad5cd430b
stevosegal <135548120+stevosegal@users.noreply.github.com>
2024-01-20 20:45:25 +1100

    Update LuaCode.lua

--- Asseto/LuaCode.lua
+++ Asseto/LuaCode.lua
@@ -1,9 +1,9 @@
 local screensize = vec2(ac.getSim().windowWidth,ac.getSim().windowHeight)
-local timer = 10
+local timer = 5
 
 --image_0 is used as the rules splash screen
 local image_0 = {
-    ['src'] = 'https://i.imgur.com/jWMAK4a.png',
+    ['src'] = 'https://i.imgur.com/CPDBeI4.gif',
     ['sizeX'] = 546, --size of your image in pixels
     ['sizeY'] = 741, --size of your image in pixels
     ['paddingX'] = screensize.x/2-546/2, --this makes it sit in the centre of the screen
